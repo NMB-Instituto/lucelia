47cf6723a1530cce33085c0c96269a3cf5ed9aa6
Ryann Ferreira <ryannnkl@gmail.com>
2023-10-21 15:07:10 -0300

    update links

--- app/components/Products/index.tsx
+++ app/components/Products/index.tsx
@@ -13,42 +13,54 @@ export default function Products() {
 			<section className="flex bg-orange-100 py-12 " id="products">
 				<Container>
 					<div className="flex gap-12 flex-col md:flex-row">
-						<div className="flex flex-col items-center gap-5">
-							<Image
-								src="/banner1.png"
-								className="rounded"
-								width={1050}
-								height={1350}
-								alt="Banner compra 1"
-							/>
-							<span className="text-2xl text-zink-700 font-bold bg-white px-6 py-2 rounded">
-								R$ 97,00
-							</span>
-						</div>
-						<div className="flex flex-col items-center gap-5">
-							<Image
-								src="/banner2.png"
-								className="rounded"
-								width={1050}
-								height={1350}
-								alt="Banner compra 1"
-							/>
-							<span className="text-2xl text-zink-700 font-bold bg-white px-6 py-2 rounded">
-								R$ 497,00
-							</span>
-						</div>
-						<div className="flex flex-col items-center gap-5">
-							<Image
-								src="/banner3.png"
-								className="rounded"
-								width={1050}
-								height={1350}
-								alt="Banner compra 1"
-							/>
-							<span className="text-2xl text-zink-700 font-bold bg-white px-6 py-2 rounded">
-								R$ 1297,00
-							</span>
-						</div>
+						<Link
+							href="https://nmbinstituto.com.br/plano/lucrando-com-lives-652d8eb668486"
+							passHref>
+							<div className="flex flex-col items-center gap-5">
+								<Image
+									src="/banner1.png"
+									className="rounded w-96 md:w-auto"
+									width={1050}
+									height={1350}
+									alt="Banner compra 1"
+								/>
+								<span className="text-2xl text-zink-700 font-bold bg-white px-6 py-2 rounded">
+									R$ 97,00
+								</span>
+							</div>
+						</Link>
+						<Link
+							href="https://nmbinstituto.com.br/plano/consultoria-online-lucelia-abreu-652d3dea35922"
+							passHref>
+							<div className="flex flex-col items-center gap-5">
+								<Image
+									src="/banner2.png"
+									className="rounded  w-96 md:w-auto"
+									width={1050}
+									height={1350}
+									alt="Banner compra 1"
+								/>
+								<span className="text-2xl text-zink-700 font-bold bg-white px-6 py-2 rounded">
+									R$ 497,00
+								</span>
+							</div>
+						</Link>
+						<Link
+							href="https://nmbinstituto.com.br/plano/consultoria-presencial-lucelia-abreu-652d842b01372"
+							passHref>
+							<div className="flex flex-col items-center gap-5">
+								<Image
+									src="/banner3.png"
+									className="rounded  w-96 md:w-auto"
+									width={1050}
+									height={1350}
+									alt="Banner compra 1"
+								/>
+								<span className="text-2xl text-zink-700 font-bold bg-white px-6 py-2 rounded">
+									R$ 1297,00
+								</span>
+							</div>
+						</Link>
 					</div>
 				</Container>
 			</section>
